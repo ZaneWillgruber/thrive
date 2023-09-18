c5750c21eebb39c262ca06536496cba2bfbaf853
Zane Willgruber <zanetwill@gmail.com>
2023-09-17 21:29:06 -0500

    structure

---
+++ thrive/pages/index.js
@@ -0,0 +1,19 @@
+//import logo from '../images/thrive_logo.jpg';
+
+const Home = () => {
+  return (
+    <div>
+      <ul>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Welcome</li>
+        <li>Join</li>
+        <li>Home</li>
+      </ul>
+      <img src="../images/thrive_logo.webp" alt="hero" />
+      <h2>Live Your Best Life</h2>
+    </div>
+  );
+};
+
+export default Home;
